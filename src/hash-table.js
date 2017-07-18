c66bc38c12507f246af944f37c1e75905f05e554
Jesse <jesse.ranon@gmail.com>
2017-07-17 23:12:40 -0700

    completed stack, queue, linked-lists

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,6 +7,25 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+  insert(key, value) {
+    if (!this[key]) this[key] = value;
+    else {
+      const cache = [[key, this[key]]];
+      cache.push([key, value]);
+      this[key] = cache;
+    }
+    this.resize();
+  }
+  remove(key) {
+    if (this[key]) delete this[key];
+  }
+  retrieve(key) {
+    return this[key];
+  }
+  resize() {
+    const keys = Object.keys(this);
+    if (keys.length >= (this.limit * 0.75)) this.limit *= 2;
+  }
 }
 
 module.exports = HashTable;
--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,38 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(x) {
+    const prop = Object.keys(this).length - 1;
+    this[prop] = { value: x };
+    this.reassign();
+  }
+  removeHead() {
+    const keys = Object.keys(this);
+    const cache = [];
+    if (keys.length > 2) {
+      cache.push(this[keys[0]]);
+      delete this[keys[0]];
+    }
+    this.reassign();
+    if (cache.length !== 0) return cache.pop().value;
+  }
+  contains(y) {
+    const val = Object.values(this);
+    for (let i = 0; i < val.length; i++) {
+      if (val[i] && val[i].value === y) return true;
+    }
+    return false;
+  }
+  reassign() {
+    const after = Object.keys(this);
+    if (after.length > 2) {
+      this.head = this[after[0]];
+      this.tail = this[after[after.length - 3]];
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+  }
 }
 
 module.exports = LinkedList;
