d3330eac143df582d2f4ad7776d89689974c9cb2
Jesse <jesse.ranon@gmail.com>
2017-07-17 20:01:54 -0700

    completed stack.js and queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.items = [];
+  }
+  enqueue(x) {
+    this.items.push(x);
+  }
+  dequeue() {
+    return this.items.shift();
+  }
+  get size() {
+    return this.items.length;
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,16 @@
 class Stack {
-
+  constructor() {
+    this.items = [];
+  }
+  add(x) {
+    this.items.push(x);
+  }
+  remove() {
+    return this.items.pop();
+  }
+  get size() {
+    return this.items.length;
+  }
 }
 
 module.exports = Stack;
